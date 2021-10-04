d24102052f7333e1fd17399b563c5a1ff211b9f2
cristian <crperezp@unal.edu.co>
2021-10-03 21:52:31 -0500

    controlador de vista usuario

--- controlador/menu.js
+++ controlador/menu.js
@@ -1,10 +1,31 @@
-var menu = new Vue({
-    el: '#menu',
+var usuario = new Vue({
+    el: '#wrapper',
     data() {
         return{
             class_img: 'img-user-style',
             class_email_text: 'email-text text-light',
-            class_username_text: 'username-text text-light',                       
+            class_username_text: 'username-text text-light',           
+            busqueda_principa: '',
+            titulo : '',
+            autor: '',
+            plataforma: '',
+            fecha_inicio: '',
+            fecha_fin: '', 
+            descripcion: '',
+            enlace: '',
+            select:'',
+            button: {
+                text: 'Agregar'
+            },
+            cursos: [
+                
+                ['Titulo curso 1', '0'],
+                ['Titulo curso 2', '1'],
+                ['Titulo curso 3', '2'],
+                ['Titulo curso 4', '3'],
+                ['Titulo curso 5', '4'],
+                
+            ]           
         }
     
     },
@@ -20,11 +41,31 @@ var menu = new Vue({
                 this.class_email_text = 'email-text text-light';
                 this.class_username_text = 'username-text text-light';
             }
+        },
+        seleccion(curso){
+            this.titulo = this.cursos[curso][0];
+            this.button.text = "Editar";
+            select = curso;
+            
+        },
+        editar_agregar(){
+            if(this.button.text == "Editar"){
+                this.cursos[select][0] = this.titulo;
+                console.log(this.cursos[select][0])
+            }else{
+                console.log("añadido");
+                this.cursos.push([this.titulo, '5']);
+            }
+            
+        },
+        boton_agregar(){
+            this.titulo = '';
+            this.button.text = "Agregar";
         }
     },
     created: function(){
-        console.log('menu creado');
+        console.log('usuario creado');
     }
 
 })
-menu.$mount('#menu')
+usuario.$mount('#wrapper')
