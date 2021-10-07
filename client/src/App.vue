3d80e07f99eb3190205b16332e54c64cde72c662
cristian <crperezp@unal.edu.co>
2021-10-07 01:25:52 -0500

    quitar v-model

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu/>
+    <BodyIndex/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BodyIndex from './components/bodyIndex.vue'
+import Menu from './components/menu.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu,
+    BodyIndex
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
